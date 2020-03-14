515c32a0350bdda6df9de3a7f550bbf55473d061
Konrad Bochnia <kbochnia@gmail.com>
2020-03-14 13:17:56 +0100

    Add measuring the time of 'sort' and 'copy' functions

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/times.h>
+#include <unistd.h>
 #include "lib_records.h"
 #include "sys_records.h"
 
@@ -16,35 +18,52 @@ void generate(char* filename, int records_count, int record_size) {
     free(buffer);
 }
 
+void print_time(struct tms* start) {
+    struct tms end;
+    times(&end);
+    printf("user: %f, sys: %f\n\n",
+           (double)(end.tms_utime - start->tms_utime) / sysconf(_SC_CLK_TCK),
+           (double)(end.tms_stime - start->tms_stime) / sysconf(_SC_CLK_TCK));
+}
+
 int main(int argc, char* args[]) {
+    struct tms cmd_start;
+
     for (int i = 1; i < argc; i++) {
+        times(&cmd_start);
+
         char* cmd = args[i];
 
         if (strcmp(cmd, "generate") == 0) {
             char* filename = args[i + 1];
             int records_count = atoi(args[i + 2]);
             int record_size = atoi(args[i + 3]);
+            i += 3;
 
             generate(filename, records_count, record_size);
-            i += 3;
         } else if (strcmp(cmd, "sort") == 0) {
             char* filename = args[i + 1];
             int records_count = atoi(args[i + 2]);
             int record_size = atoi(args[i + 3]);
             char* type = args[i + 4];
+            i += 4;
 
             if (strcmp(type, "sys") == 0) {
                 sys_sort(filename, records_count, record_size);
             } else {
                 lib_sort(filename, records_count, record_size);
             }
-            i += 4;
+
+            printf("sort %s %d %d %s:\n", filename, records_count, record_size,
+                   type);
+            print_time(&cmd_start);
         } else if (strcmp(cmd, "copy") == 0) {
             char* src_filename = args[i + 1];
             char* dst_filename = args[i + 2];
             int records_count = atoi(args[i + 3]);
             int record_size = atoi(args[i + 4]);
             char* type = args[i + 5];
+            i += 5;
 
             if (strcmp(type, "sys") == 0) {
                 sys_copy(src_filename, dst_filename, records_count,
@@ -53,7 +72,10 @@ int main(int argc, char* args[]) {
                 lib_copy(src_filename, dst_filename, records_count,
                          record_size);
             }
-            i += 5;
+
+            printf("copy %s %s %d %d %s:\n", src_filename, dst_filename,
+                   records_count, record_size, type);
+            print_time(&cmd_start);
         } else {
             fprintf(stderr, "Invalid command: %s", cmd);
             return 1;
