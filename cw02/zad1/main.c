bbc4ab8e63a474532c351ab490feecea977320cf
Konrad Bochnia <kbochnia@gmail.com>
2020-03-14 11:03:00 +0100

    Add command-line argument parsing

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -17,7 +17,48 @@ void generate(char* filename, int records_count, int record_size) {
 }
 
 int main(int argc, char* args[]) {
-    generate("tmp", 10, 2);
-    sys_copy("tmp", "tmp2", 10, 2);
-    sys_sort("tmp", 10, 2);
+    for (int i = 1; i < argc; i++) {
+        char* cmd = args[i];
+
+        if (strcmp(cmd, "generate") == 0) {
+            char* filename = args[i + 1];
+            int records_count = atoi(args[i + 2]);
+            int record_size = atoi(args[i + 3]);
+
+            generate(filename, records_count, record_size);
+            i += 3;
+        } else if (strcmp(cmd, "sort") == 0) {
+            char* filename = args[i + 1];
+            int records_count = atoi(args[i + 2]);
+            int record_size = atoi(args[i + 3]);
+            char* type = args[i + 4];
+
+            if (strcmp(type, "sys") == 0) {
+                sys_sort(filename, records_count, record_size);
+            } else {
+                lib_sort(filename, records_count, record_size);
+            }
+            i += 4;
+        } else if (strcmp(cmd, "copy") == 0) {
+            char* src_filename = args[i + 1];
+            char* dst_filename = args[i + 2];
+            int records_count = atoi(args[i + 3]);
+            int record_size = atoi(args[i + 4]);
+            char* type = args[i + 5];
+
+            if (strcmp(type, "sys") == 0) {
+                sys_copy(src_filename, dst_filename, records_count,
+                         record_size);
+            } else {
+                lib_copy(src_filename, dst_filename, records_count,
+                         record_size);
+            }
+            i += 5;
+        } else {
+            fprintf(stderr, "Invalid command: %s", cmd);
+            return 1;
+        }
+    }
+
+    return 0;
 }
