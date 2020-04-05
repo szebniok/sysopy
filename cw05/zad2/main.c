73d886f98376fc529c2309e2556c21b82b661e34
Konrad Bochnia <kbochnia@gmail.com>
2020-04-05 21:08:31 +0200

    Solve 2

---
+++ cw05/zad2/main.c
@@ -0,0 +1,24 @@
+#define _POSIX_C_SOURCE 2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./main file\n");
+        return 1;
+    }
+
+    char* cmd = calloc(strlen(argv[1]) + 10, sizeof(char));
+    sprintf(cmd, "sort %s", argv[1]);
+    free(cmd);
+
+    FILE* output = popen(cmd, "w");
+
+    char buffer[257];
+    while (fgets(buffer, sizeof(buffer) - 1, output) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(output);
+}
