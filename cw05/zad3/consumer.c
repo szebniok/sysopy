5c6e0df73a4cccc341ef13dd47c74e84507e7c81
Konrad Bochnia <kbochnia@gmail.com>
2020-04-06 11:39:03 +0200

    Solve 3

---
+++ cw05/zad3/consumer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./consumer pipe file N\n");
+        return 1;
+    }
+
+    srand(time(NULL));
+
+    FILE *pipe = fopen(argv[1], "r");
+    FILE *file = fopen(argv[2], "w+");
+    const int N = atoi(argv[3]);
+
+    char *buffer = calloc(N + 1, sizeof(char));
+    while (fread(buffer, 1, N, pipe) > 0) {
+        fwrite(buffer, 1, N, file);
+    }
+
+    free(buffer);
+    fclose(file);
+    fclose(pipe);
+}
---
+++ cw05/zad3/producer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: ./producer pipe file N\n");
+        return 1;
+    }
+
+    srand(time(NULL));
+
+    FILE *pipe = fopen(argv[1], "w");
+    FILE *file = fopen(argv[2], "r");
+    const int N = atoi(argv[3]);
+
+    char *buffer = calloc(N + 1, sizeof(char));
+    while (fread(buffer, 1, N, file) > 0) {
+        sleep(rand() % 2 + 1);
+        fprintf(pipe, "#%d#%s\n", getpid(), buffer);
+        fflush(pipe);
+    }
+
+    free(buffer);
+    fclose(file);
+    fclose(pipe);
+}
